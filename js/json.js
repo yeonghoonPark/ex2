3ff1b9cbc32ccc0051d05f4d6b9ebdf4d6410b30
yeonghoon Park <huklmnj2@naver.com>
2022-06-03 17:48:35 +0900

    JSON

---
+++ js/json.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// JSON
+// javaScript object Notaion
+
+// 1. Object to JSON
+// stringfy(object)
+// object를 JSON으로 변환할 때 object에 함수가 있더라도 JSON에는 전달되지 않는다.
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${this.name} can jump!`);
+    }
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name', 'color']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name'? 'gildong' : value;
+});
+console.log(json);
+
+
+// 2. JSON to Object
+// parse(json)
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate()); 
+
+
